d9ab64dfa88f4a8bc9bc313a254f11b6ed0ac19a
ulisses.bonfim <ulissesmb@gmail.com>
2020-12-22 19:11:12 -0300

    habilitando profile

--- src/main/java/com/ulissesmb/config/SwaggerConfig.java
+++ src/main/java/com/ulissesmb/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@Profile("prod")
+@Profile("dev")
 @EnableSwagger2
 public class SwaggerConfig {
 
